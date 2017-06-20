35243245059fedddbcad721587cc853f00253f70
viniRinaldis <vinireato@gmail.com>
2017-06-19 21:00:21 -0300

    added sql file

---
+++ database.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE miniprojeto;
+
+
+
+
+
+use miniprojeto;
+
+
+
+
+
+CREATE TABLE lista_de_compras(
+    nome_produto VARCHAR(80),
+        quantidade INTEGER
+      );
+
+
+
+
+
+insert into lista_de_compras (nome_produto, quantidade) values("produto2", 4);
+
+
+
+
+select * from lista_de_compras;
